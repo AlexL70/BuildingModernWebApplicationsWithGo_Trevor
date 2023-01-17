b314390de26567cc4a51b965dcc11d8af0a85f05
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-17 12:55:11 +0200

    10. Types and Structs

--- learning-go/main.go
+++ learning-go/main.go
@@ -1,23 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
-	fmt.Println("Hello, world.")
+var s = "seven"
 
-	var whatToSay string
-	var i int
-
-	whatToSay = "Goodbye, cruel world."
-	fmt.Println(whatToSay)
-
-	i = 7
-	fmt.Println("i is set to", i)
-
-	whatWasSaid, theOtherThingThatWasSaid := saySomething()
-	fmt.Println(whatWasSaid, theOtherThingThatWasSaid, "was said.")
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
 }
 
-func saySomething() (string, string) {
-	return "something", "else"
+func main() {
+	s2 := "six"
+	s := "eight"
+
+	fmt.Printf("s is %q\n", s)
+	fmt.Printf("s2 is %q\n", s2)
+
+	saySomething("xxx")
+
+	user := User{
+		FirstName:   "Trevor",
+		LastName:    "Sawler",
+		PhoneNumber: "11111111",
+		Age:         50,
+	}
+
+	fmt.Printf("User Name is %q and last name is %q and birthdate is %q\n", user.FirstName, user.LastName, user.BirthDate)
+}
+
+func saySomething(s3 string) (string, string) {
+	fmt.Printf("s inside saySomething function is %q\n", s)
+	return s3, "World"
 }
