fab3674c739fa53f25252aa6c8c4557dff2b1a01
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-18 12:19:13 +0200

    23. Functions and handlers

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "This is a home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(3, 2)
+	_, _ = fmt.Fprintf(w, "This is an about page. And 3 + 2 is %d", sum)
+}
+
+// addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, Web!")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		fmt.Printf("Number of bytes written is %d\n", n)
-	})
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Starting Web on port %s\n", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
