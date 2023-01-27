28c46ddbeeccf501776b55a61af94bef8cf07f85
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-27 17:59:06 +0200

    75. Writing tests for our POST handlers

--- bookings/internal/handlers/handlers_test.go
+++ bookings/internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -25,6 +26,20 @@ var theTests = []struct {
 	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"mkres", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"post-sa", "/search-availability", "POST", []postData{
+		{key: "start", value: "2024-01-01"},
+		{key: "end", value: "2024-01-05"},
+	}, http.StatusOK},
+	{"post-sa-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2024-01-01"},
+		{key: "end", value: "2024-01-05"},
+	}, http.StatusOK},
+	{"post-mr", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "John"},
+		{key: "last_name", value: "Smith"},
+		{key: "email", value: "john.smith@example.com"},
+		{key: "phone", value: "1111-222-333"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -42,7 +57,17 @@ func TestHandlers(t *testing.T) {
 				t.Errorf("%s: bad status code. Expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
 		} else {
-
+			values := url.Values{}
+			for _, param := range e.params {
+				values.Add(param.key, param.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Errorf("%s: error running request: %q", e.name, err)
+			}
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("%s: bad status code. Expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
