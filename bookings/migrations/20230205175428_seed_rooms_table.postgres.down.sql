2f80f7ea1279aa9ab1c187c418218a42d3b14661
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-02-05 20:11:46 +0200

    118. Adding a migration for seeding restrictions

--- bookings/migrations/20230205175428_seed_rooms_table.postgres.down.sql
+++ bookings/migrations/20230205175428_seed_rooms_table.postgres.down.sql
@@ -1,1 +1,1 @@
-delete from rooms;
+delete from public.rooms;
---
+++ bookings/migrations/20230205180735_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.restrictions
+    (id, restriction_name,created_at,updated_at)
+VALUES
+    (1, 'Reservation', '2023-02-01 00:00:00.000', '2023-02-01 00:00:00.000'),
+    (2, 'Owners'' Block', '2023-02-03 00:00:00.000', '2023-02-03 00:00:00.000');
