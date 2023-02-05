2e9172d8ffd59dffca491cc2f6a7771ed85591b9
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-02-05 20:07:02 +0200

    117. Adding a migration for seeding rooms

---
+++ bookings/migrations/20230205175428_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (id, room_name,created_at,updated_at) VALUES
+	 (1, 'General''s Quoters','2023-02-01 00:00:00.000','2023-02-01 00:00:00.000'),
+	 (2, 'Major''s Suite','2023-02-01 00:00:00.000','2023-02-01 00:00:00.000');
