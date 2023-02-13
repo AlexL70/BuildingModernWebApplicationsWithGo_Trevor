c77dbdd78bd9614a3e810eb9e11d3b4e7b2e599b
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-02-13 20:06:07 +0200

    137. Creating our handler to log in

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -428,4 +429,34 @@ func (m *Repository) ShowLogin(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "login.page.gohtml", &models.TemplateData{
 		Form: forms.New(nil),
 	})
+}
+
+// PostShowLogin handles logging the user in
+func (m *Repository) PostShowLogin(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.RenewToken(r.Context())
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("email", "password")
+	form.IsEmail("email")
+	if form.Valid() {
+		// TODO return back to login page
+	}
+
+	email := form.Get("email")
+	password := form.Get("password")
+	id, _, err := m.DB.Authenticate(email, password)
+	if err != nil {
+		log.Println(err)
+		m.App.Session.Put(r.Context(), "error", "Invalid login!")
+		http.Redirect(w, r, "/user-login", http.StatusSeeOther)
+		return
+	}
+
+	m.App.Session.Put(r.Context(), "user_id", id)
+	m.App.Session.Put(r.Context(), "flash", "Successful login!")
+	http.Redirect(w, r, "/", http.StatusOK)
 }
