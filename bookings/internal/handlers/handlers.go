e2d452aa739a73619d9b234bf456d00cbf9a18bb
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-02-14 11:52:37 +0200

    142. Protecting our routes with authentication middleware

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -470,3 +470,7 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 	_ = m.App.Session.RenewToken(r.Context())
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
+
+func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-dashboard.page.gohtml", &models.TemplateData{})
+}
