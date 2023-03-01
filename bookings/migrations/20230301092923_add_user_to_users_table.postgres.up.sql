135d5b98086a095f386db68edfbfffeff9dac00b
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-01 11:40:25 +0200

    171. Add admin user to migrations

---
+++ bookings/migrations/20230301092923_add_user_to_users_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.users
+    (first_name,last_name,email,"password",access_level,created_at,updated_at)
+VALUES
+    ('Alexander', 'Levinson', 'alexl@email.com', '$2a$10$z1i3zhG6QYqJawQDpCEegeoBveKNM8i7oHaqUWeHJ7sW1Af4rwNea', 3, '2023-03-23 00:00:00', '2023-03-23 00:00:00');
